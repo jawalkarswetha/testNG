9de7bc53b744a63e9b5d960094b94b98cf09a4f6
jawalkarswetha <jawalkarswetha99@gmail.com>
2025-05-25 15:16:21 +0530

    commiting my changes to branchX

---
+++ src/dummyTest.java
@@ -0,0 +1,12 @@
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class dummyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("dummt file");
+
+	}
+
+}
--- src/testNG/dataProvider6.java
+++ src/testNG/dataProvider6.java
@@ -1,4 +1,4 @@
-package testNG;
+ package testNG;
 
 import java.util.ArrayList;
 import java.util.Iterator;
